7c7f3a81c8d7ebc9b82e69745cbeab913f376a35
trungkien-web <55096570+trungkien-web@users.noreply.github.com>
2019-10-10 16:34:13 +0700

    DUCDD_Commit Code
    Thêm controller để test view phần của Tùng

---
+++ Code/RecruimentSystem/RecruimentSystem/Controllers/Recruiter/RecruiterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruitmentSystem.Controllers.Recruiter
+{
+    public class RecruiterController : Controller
+    {
+        // GET: SearchStudent
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult SearchStudent()
+		{
+			return View();
+		}
+		public ActionResult PublishedRecruitment()
+		{
+			return View();
+		}
+	}
+}
