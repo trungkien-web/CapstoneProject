a31d2b03bf1aadcc88b8f2220de91f736352b666
trungkien-web <55096570+trungkien-web@users.noreply.github.com>
2019-10-22 17:44:30 +0700

    DUCDD Commit Code

---
+++ Code/FPT Internship Management System/FPTInternshipManagement/Service/Skill/ISkillService.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+	public interface ISkillService
+	{
+		List<Skill> GetAllSkills();
+		Skill GetSkillById(int id);
+		List<Skill> GetSkillsByDepartmentId(int id);
+	}
+}
---
+++ Code/FPT Internship Management System/FPTInternshipManagement/Service/Skill/SkillService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Repository;
+
+namespace Service
+{
+	public class SkillService : ISkillService
+	{
+		ISkillRepository repository = new SkillRepository();
+		public List<Skill> GetAllSkills()
+		{
+			return repository.GetAllSkills();
+		}
+
+		public Skill GetSkillById(int id)
+		{
+			return repository.GetSkillById(id);
+		}
+
+		public List<Skill> GetSkillsByDepartmentId(int id)
+		{
+			return repository.GetSkillsByDepartmentId(id);
+		}
+	}
+}
