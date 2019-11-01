2affeef144b8eb310589074e8728c820c0cb5fa5
trungkien-web <55096570+trungkien-web@users.noreply.github.com>
2019-11-01 11:14:47 +0700

    HoaNH2 update source service about student

--- Code/FPT Internship Management System/FPTInternshipManagement/Service/Recruiter/IRecruiterService.cs
+++ Code/FPT Internship Management System/FPTInternshipManagement/Service/Recruiter/IRecruiterService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
-namespace Service.Recruiter
+namespace Service
 {
     public interface IRecruiterService
     {
+        List<User> searchStudents(DateTime startDate, DateTime endDate, string studentID, string location, string studentName);
+
+        void InsertStudent(User newStudent);
+
+        void UpdateStudent(User newStudent);
     }
 }
--- Code/FPT Internship Management System/FPTInternshipManagement/Service/Recruiter/RecruiterService.cs
+++ Code/FPT Internship Management System/FPTInternshipManagement/Service/Recruiter/RecruiterService.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using Repository;
 
-namespace Service.Recruiter
+namespace Service
 {
-    public class RecruiterService
+    public class RecruiterService:IRecruiterService
     {
+        IUserRepository userRepo = new UserRepository();
+
+        public List<User> searchStudents(DateTime startDate, DateTime endDate, string studentID, string location, string studentName)
+        {
+            if (startDate.Equals(null) && endDate.Equals(null) && string.IsNullOrEmpty(studentID) && string.IsNullOrEmpty(location) && string.IsNullOrEmpty(studentName))
+            {
+                return userRepo.GetAllStudents();
+            }
+
+            if (startDate.Equals(null) && endDate.Equals(null) && !string.IsNullOrEmpty(studentID) && string.IsNullOrEmpty(location) && string.IsNullOrEmpty(studentName))
+            {
+                return userRepo.SearchStudentsWithId(int.Parse(studentID));
+            }
+
+            if (startDate.Equals(null) && endDate.Equals(null) && !string.IsNullOrEmpty(studentID) && !string.IsNullOrEmpty(location) || !string.IsNullOrEmpty(studentName))
+            {
+                return userRepo.SearchStudents(location, int.Parse(studentID), studentName);
+            }
+
+            if (!startDate.Equals(null) && !endDate.Equals(null) && string.IsNullOrEmpty(studentID) && string.IsNullOrEmpty(location) && string.IsNullOrEmpty(studentName))
+            {
+                return userRepo.SearchStudentsInTime(startDate, endDate);
+            }
+
+            if (!startDate.Equals(null) && !endDate.Equals(null) && !string.IsNullOrEmpty(studentID) && !string.IsNullOrEmpty(location) && !string.IsNullOrEmpty(studentName))
+            {
+                return userRepo.SearchStudentsDetail(startDate, endDate, location, int.Parse(studentID), studentName);
+            }
+
+            return null;
+        }
+
+        public void InsertStudent(User newStudent)
+        {
+            userRepo.InsertStudent(newStudent);
+        }
+
+        public void UpdateStudent(User newStudent)
+        {
+            userRepo.UpdateStudent(newStudent);
+        }
     }
 }
