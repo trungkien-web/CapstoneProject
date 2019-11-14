ba8f28aafc91ddf65c4c918130c59fef287bc8f9
trungkien-web <55096570+trungkien-web@users.noreply.github.com>
2019-11-14 13:19:31 +0700

    DUCDD Commit Code

---
+++ Code/FPT Internship Management System/FPTInternshipManagement/Repository/Job/JobRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Repository
+{
+	public class JobRepository : IJobRepository
+	{
+		FPTInternshipManagermentEntities ctx = new FPTInternshipManagermentEntities();
+		public List<Job> GetAllJobs()
+		{
+			var jobs = ctx.Jobs.ToList();
+			return jobs;
+		}
+
+	}
+}
