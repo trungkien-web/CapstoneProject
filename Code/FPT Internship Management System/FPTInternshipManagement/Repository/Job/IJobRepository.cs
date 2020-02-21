dfa3c67a22f05b783a23d53fb8f9ee09d5e755e1
nguyenhuuhoa <nguyenhoa26051995@gmail.com>
2020-02-21 09:13:36 +0700

    hoanh2 update interface

--- Code/FPT Internship Management System/FPTInternshipManagement/Repository/Job/IJobRepository.cs
+++ Code/FPT Internship Management System/FPTInternshipManagement/Repository/Job/IJobRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//HoaNA update interface
 namespace Repository
 {
 	public interface IJobRepository
